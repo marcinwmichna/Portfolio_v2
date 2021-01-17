4dedd6399644c97d40fad6e419523ad0edb892c2
marcinwmichna <marcin.w.michna@gmail.com>
2021-01-17 18:24:08 +0100

    Horizontal projects block with images, descriptions. Buttons alligment to improve.

--- src/components/RecentProjectsAndDesigns.vue
+++ src/components/RecentProjectsAndDesigns.vue
@@ -6,53 +6,57 @@
         msg="RECENT PROJECTS AND DESIGNS"
       ></SectionHeader>
       <div class="blockWrapper">
-        <RecentProjectsAndDesignsBlock
+        <RecentProjectsAndDesignsBlockHorizontal
           upHeader="THE WEATHERR"
           mainHeader="WEATHER APPLICATION"
           subHeader="MY LATEST WORK. CLEAN AND SIMPLE. MADE WITH VUEJS & VUEX USING OPEN WEATHER API. DESIGNED IN FIGMA."
           viewDesignButton="VIEW WEBSITE"
           urlToImage="theWeatherr.png"
-        ></RecentProjectsAndDesignsBlock>
-        <RecentProjectsAndDesignsBlock
+        ></RecentProjectsAndDesignsBlockHorizontal>
+        <RecentProjectsAndDesignsBlockHorizontal
           mainHeader="RESUME"
           subHeader="MY OWN RESUME. INCLUDES MY SKILLSET, CONTACT INFORMATION AND WORK EXPERIENCE. CLEAN AND SIMPLE. DESIGNED IN FIGMA."
           viewDesignButton="VIEW DESIGN"
           viewWebsiteButton="VIEW WEBSITE"
           urlToImage="resume.png"
-        ></RecentProjectsAndDesignsBlock>
-        <RecentProjectsAndDesignsBlock
-          mainHeader="RESUME"
-          subHeader="MY OWN RESUME. INCLUDES MY SKILLSET, CONTACT INFORMATION AND WORK EXPERIENCE. CLEAN AND SIMPLE. DESIGNED IN FIGMA."
-          viewDesignButton="VIEW DESIGN"
-          viewWebsiteButton="VIEW WEBSITE"
-          urlToImage="resume.png"
-        ></RecentProjectsAndDesignsBlock>
-        <RecentProjectsAndDesignsBlock
-          mainHeader="RESUME"
-          subHeader="MY OWN RESUME. INCLUDES MY SKILLSET, CONTACT INFORMATION AND WORK EXPERIENCE. CLEAN AND SIMPLE. DESIGNED IN FIGMA."
-          viewDesignButton="VIEW DESIGN"
-          viewWebsiteButton="VIEW WEBSITE"
-          urlToImage="resume.png"
-        ></RecentProjectsAndDesignsBlock>
-        <RecentProjectsAndDesignsBlock
-          mainHeader="RESUME"
-          subHeader="MY OWN RESUME. INCLUDES MY SKILLSET, CONTACT INFORMATION AND WORK EXPERIENCE. CLEAN AND SIMPLE. DESIGNED IN FIGMA."
-          viewDesignButton="VIEW DESIGN"
-          viewWebsiteButton="VIEW WEBSITE"
-          urlToImage="resume.png"
-        ></RecentProjectsAndDesignsBlock>
+        ></RecentProjectsAndDesignsBlockHorizontal>
+        <div class="verticalBlocksWrapper">
+          <RecentProjectsAndDesignsBlockVertical
+            upHeader="PERSONAL PORTFOLIO"
+            mainHeader="PORTFOLIO WEBSITE "
+            subHeader="PREVIOUS PERSONAL PORTFOLIO WEBSITE MADE USING VANILLA JS, VANILLA HTML & CSS. EMAIL SENDER HANDLED WITH NODEJS."
+            viewDesignButton="VIEW WEBSITE"
+            urlToImage="portfolioWebsite.png"
+          ></RecentProjectsAndDesignsBlockVertical>
+          <RecentProjectsAndDesignsBlockVertical
+            upHeader="TO BUY"
+            mainHeader="SHOPPING APPLICATION"
+            subHeader="CLEAN AND INTUITIVE SHOPPING LIST WEBSITE. MADE WITH VANILLA JS, HTML & CSS."
+            viewDesignButton="VIEW WEBSITE"
+            urlToImage="shoppingApplication.png"
+          ></RecentProjectsAndDesignsBlockVertical>
+          <RecentProjectsAndDesignsBlockVertical
+            upHeader="BET IT OUT"
+            mainHeader="VIRTUAL SPORT BETTING"
+            subHeader="APPLICATION CONCEPT BASED ON VIRTUAL SPORTS BETTING IDEA. DESIGNED IN FIGMA."
+            viewDesignButton="VIEW DESIGN"
+            urlToImage="betItOut.png"
+          ></RecentProjectsAndDesignsBlockVertical>
+        </div>
       </div>
     </div>
   </section>
 </template>
 <script>
 import SectionHeader from "@/components/SectionHeader.vue";
-import RecentProjectsAndDesignsBlock from "@/components/RecentProjectsAndDesigns_block.vue";
+import RecentProjectsAndDesignsBlockHorizontal from "@/components/RecentProjectsAndDesigns_block_horizontal.vue";
+import RecentProjectsAndDesignsBlockVertical from "@/components/RecentProjectsAndDesigns_block_vertical.vue";
 
 export default {
   components: {
     SectionHeader,
-    RecentProjectsAndDesignsBlock,
+    RecentProjectsAndDesignsBlockHorizontal,
+    RecentProjectsAndDesignsBlockVertical,
   },
 };
 </script>
@@ -64,6 +68,13 @@ export default {
   margin: auto;
   .title {
     margin-top: 7rem;
+  }
+  .blockWrapper {
+    .verticalBlocksWrapper {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+    }
   }
 }
 @media screen and (max-width: 1400px) {
---
+++ src/components/RecentProjectsAndDesigns_block_vertical.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="block">
+    <picture class="image_block">
+      <img :src="require(`../assets/${urlToImage}`)" alt="" />
+    </picture>
+    <div class="description_block">
+      <div class="descriptionWrapper">
+        <SectionSubheader
+          class="projectName"
+          :msg="upHeader"
+        ></SectionSubheader>
+        <NavDescription class="mainHeader" :msg="mainHeader"></NavDescription>
+        <SectionSubheader
+          class="description"
+          :msg="subHeader"
+        ></SectionSubheader>
+      </div>
+      <div class="buttonsWrapper">
+        <RegularButton
+          class="viewButton"
+          :msg="viewDesignButton"
+        ></RegularButton>
+        <RegularButton
+          class="viewButton"
+          :msg="viewWebsiteButton"
+        ></RegularButton>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import NavDescription from "@/components/NavDescription.vue";
+import SectionSubheader from "@/components/SectionSubheader.vue";
+import RegularButton from "@/components/RegularButton.vue";
+
+export default {
+  props: {
+    upHeader: String,
+    mainHeader: String,
+    subHeader: String,
+    viewDesignButton: String,
+    viewWebsiteButton: String,
+    urlToImage: URL,
+  },
+  components: {
+    NavDescription,
+    RegularButton,
+    SectionSubheader,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.block {
+  --width: 31vw;
+  margin-top: 2rem;
+  // display: flex;
+  // flex-direction: column;
+  // position: relative;
+  width: 29%;
+
+  // margin: 5px;
+  .description_block {
+    margin: auto;
+    // margin-bottom: auto;
+    margin-top: 2rem;
+    // margin-bottom: 0;
+    // height: calc(100% - 27vw);
+    height: calc(100% - var(--width));
+    justify-content: space-between;
+    // align-content: space-between;
+
+    // -webkit-align-content: space-between;
+    display: flex;
+    flex-direction: column;
+    .descriptionWrapper {
+      // margin: auto;
+    }
+    .projectName {
+      color: #424242;
+    }
+    .mainHeader {
+      margin-top: 0;
+      font-weight: bold;
+    }
+    .description {
+      margin-bottom: 1rem;
+      // margin-bottom: auto;
+    }
+    .viewButton {
+      padding: 0;
+      margin: 0.5rem 0;
+      // align-self: flex-end;
+    }
+  }
+  picture {
+    img {
+      width: 100%;
+    }
+  }
+
+  @media screen and (max-width: 1900px) {
+    .image_block img {
+      // width: 35vw;
+    }
+    picture::before {
+      height: 93%;
+    }
+  }
+  @media screen and (max-width: 1000px) {
+    picture::before {
+      // height: 22vw;
+    }
+  }
+  @media screen and (min-width: 1400px) {
+    .description_block {
+      height: calc(120% - var(--width));
+    }
+  }
+}
+</style>
